d0a2dce98ee3ecaa12b52e8c56bef5bbc1a68717
Karina <49018494+kilkirua@users.noreply.github.com>
2020-04-20 15:00:06 +0300

    Update swap_max_and_min.py

--- python/module3/swap_max_and_min.py
+++ python/module3/swap_max_and_min.py
@@ -1,10 +1,11 @@
 def swap_max_and_min(list):
     maximum = max(list)
     minimum = min(list)
-    if len(list) == len(set(list)):
-        for i in range(len(list)):
-            if list[i] == maximum:
-                list[i] = minimum
-            elif list[i] == minimum:
-                list[i] = maximum
-        return list
+    if len(list) != len(set(list)):
+        raise ValueError()
+    for i in range(len(list)):
+        if list[i] == maximum:
+            list[i] = minimum
+        elif list[i] == minimum:
+            list[i] = maximum
+    return list
