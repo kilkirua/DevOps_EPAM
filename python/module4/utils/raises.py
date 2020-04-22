4e725857c832dc88c54316074fa1bf88d52b0b2c
Karina <49018494+kilkirua@users.noreply.github.com>
2020-04-22 20:31:45 +0300

    Update raises.py

--- python/module4/utils/raises.py
+++ python/module4/utils/raises.py
@@ -1,1 +1,10 @@
-
+def raises(error):
+    def wrapper(func):
+        def return_error(*args, **kwargs):
+            try:
+                res = func(*args, **kwargs)
+                return res
+            except:
+                raise error
+        return return_error
+    return wrapper
