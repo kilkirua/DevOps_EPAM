79d8d238100239d95bea57e9444ea9d9fa3377e8
Karina <49018494+kilkirua@users.noreply.github.com>
2020-04-27 17:41:49 +0300

    Update employee.py
    add classes DevOps and Manager.

--- python/module5/employee.py
+++ python/module5/employee.py
@@ -20,3 +20,40 @@ class Employee:
     def from_str(cls, emp_str):
         first_name, last_name, salary = emp_str.split(",")
         return cls(first_name, last_name, int(salary))
+
+
+class DevOps(Employee):
+    def __init__(self, first_name, last_name, salary, skills=[]):
+        super().__init__(first_name, last_name, salary)
+        self.skills = [skill.title() for skill in skills]
+
+    def add_skill(self, skill):
+        if skill.title() not in self.skills:
+            self.skills.append(skill.title())
+
+    def remove_skill(self, skill):
+        try:
+            self.skills.remove(skill.title())
+        except ValueError as error:
+            return error
+
+
+class Manager(Employee):
+    def __init__(self, first_name, last_name, salary, subordinates=[]):
+        super().__init__(first_name, last_name, salary)
+        self.subordinates = subordinates
+
+    def add_subordinate(self, subordinate):
+        if subordinate not in self.subordinates:
+            self.subordinates.append(subordinate)
+
+    def remove_subordinate(self, subordinate):
+        if isinstance(subordinate, str):
+            for employee in self.subordinates:
+                if employee.email == subordinate:
+                    self.subordinates.remove(employee)
+        else:
+            try:
+                self.subordinates.remove(subordinate)
+            except ValueError as error:
+                return error
